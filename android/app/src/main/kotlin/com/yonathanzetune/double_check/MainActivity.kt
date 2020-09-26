ede19d6a00f4dc2bd5e485e93772ee5cf2404977
YonathanZetune <yonathan@zetune.com>
2020-09-26 11:09:41 -0500

    init files

---
+++ android/app/src/main/kotlin/com/yonathanzetune/double_check/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.yonathanzetune.double_check
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/Objects-normal/arm64/Runner_vers.d
@@ -0,0 +1,2 @@
+dependencies: \
+  /Users/YoniZetune/Develop/DoubleCheck/double_check/ios/Build/Intermediates.noindex/Runner.build/Debug-iphoneos/Runner.build/DerivedSources/Runner_vers.c
